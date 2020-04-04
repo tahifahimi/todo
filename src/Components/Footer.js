03b899c1254aac07cebc6ca65a12d0bfdf3b8aac
tahere fahimi <t.fahimi1998@gmail.com>
2020-04-04 15:28:06 +0430

    added the footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer class="footer">
+          {/* <!-- This should be `0 items left` by default --> */}
+          <span class="todo-count">
+            <strong>0</strong> item left
+          </span>
+          {/* <!-- Remove this if you don't implement routing --> */}
+          <ul class="filters">
+            <li>
+              <a class="selected" href="#/">
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          {/* <!-- Hidden if no completed items are left ↓ --> */}
+          <button class="clear-completed">Clear completed</button>
+        </footer>
+      </div>
+    );
+  }
+}
+export default Footer;
--- src/Components/TodoItems.js
+++ src/Components/TodoItems.js
@@ -1,32 +1,30 @@
 import React, { Component } from "react";
-import TodoItem from './TodoItem'
-import todoStore from '../Stores/TodoStore';
-import {observer} from 'mobx-react'
+import TodoItem from "./TodoItem";
+import todoStore from "../Stores/TodoStore";
+import { observer } from "mobx-react";
+import Footer from "./Footer";
 
 @observer
 class TodoItems extends Component {
   render() {
     return (
-        <section className="main">
-          <ul className="todo-list">
-            {
-              todoStore.todos.map(todo=>{
-                return(
-                  <TodoItem todo = {todo}/>
-                )
-              })
-            }
-            <li>
-              <div className="view">
-                <input type="checkbox" className="toggle" />
-                <label>new work</label>
-                <button className="destry" />
-              </div>
-            </li>
-  {/* <TodoItem/> */}
-          </ul>
-        </section>
+      <section className="main">
+        <ul className="todo-list">
+          {todoStore.todos.map((todo) => {
+            return <TodoItem todo={todo} />;
+          })}
+          <li>
+            <div className="view">
+              <input type="checkbox" className="toggle" />
+              <label>new work</label>
+              <button className="destry" />
+            </div>
+          </li>
+          {/* <TodoItem/> */}
+        </ul>
+        <Footer/>
+      </section>
     );
   }
 }
-export default TodoItems
+export default TodoItems;
