f4e444e10bcd19fd284897bbc7c7da3feaa56df9
tahere fahimi <t.fahimi1998@gmail.com>
2020-04-07 13:15:36 +0430

    the check box is fixed

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -4,7 +4,11 @@ import todoStore from "../Stores/TodoStore";
 
 @observer
 class TodoItems extends Component {
-  
+  constructor(props) {
+    super(props);
+    // this.state ={}
+  }
+
   toggleCheckBox = () => {
     const { todo } = this.props;
     todoStore.toggle(todo.id);
@@ -14,21 +18,25 @@ class TodoItems extends Component {
     const { todo } = this.props;
     todoStore.remove(todo.id);
   };
-  
+
   render() {
     const { todo } = this.props;
     return (
       <li className={todo.completed ? "completed" : ""}>
         <div className="view">
+          {console.log(todo.completed)}
           <input
             type="checkbox"
+            id = {todo.id}
             className="toggle"
-            value={!todo.completed}
+            value={todo.completed}
+            name ={todo.title}
             onClick={this.toggleCheckBox}
+            checked ={todo.completed}
             // onClick={this.}
           />
-          
-          <label>{todo.title }</label>
+
+          <label for={todo.title}>{todo.title}</label>
           <button className="destroy" onClick={this.removeTask} />
         </div>
       </li>
